01d5bf42221fde825db60c48dfc124b68ecd643d
Danny Cho <dannycho7@gmail.com>
2018-03-19 19:04:04 -0700

    Add level vector to intentional data race pbfs and sequential bfs for verification

--- graph.cpp
+++ graph.cpp
@@ -22,39 +22,41 @@ void Graph::addEdge(int v, int w) {
 }
 
 void Graph::BFS(int s) {
-	bool visited[this->V];
-	memset(visited, 0, this->V * sizeof(bool));
+	int levels[this->V];
+	memset(levels, 0xFF, this->V * sizeof(int));
 	std::queue<int> frontier;
 
-	frontier.push(s);
-
 	int level = 0;
+	frontier.push(s);
+	levels[s] = level;
+
 	int lastElInLevel = s;
 
 	while (!frontier.empty()) {
 		int current = frontier.front();
 		frontier.pop();
-		visited[current] = true;
-		std::cout << "level " << level << ": " << current << " adjacent edges: ";
 		for (std::list<int>::const_iterator it = this->adj[current].begin(), end = this->adj[current].end(); it != end; it++) {
-			if (!visited[*it]) {
-				std::cout << *it << " ";
-				visited[*it] = true;
+			if (levels[*it] < 0) {
 				frontier.push(*it);
+				levels[*it] = level + 1;
 			}
 
 		}
-		std::cout << std::endl;
 
 		if (current == lastElInLevel) {
 			level++;
 			lastElInLevel = frontier.back();
 		}
 	}
+
+	for (int i = 0; i < this->V; i++) {
+		std::cout << i << ": " << levels[i] << std::endl;
+	}
 }
 
 void Graph::PBFS(int s) {
 	int parents[this->V];
+	int levels[this->V];
 	memset(&parents, 0xFF, sizeof(parents));
 
 	std::vector<int> frontier;
@@ -63,21 +65,19 @@ void Graph::PBFS(int s) {
 	parents[s] = s;
 	frontier.push_back(s);
 
-	int level = 1;
+	int level = 0;
 
 	while (!frontier.empty()) {
 		cilk_for (std::vector<int>::iterator f = frontier.begin(), end = frontier.end(); f != end; f++) {
-			std::cout << "Level " << level << ". Neighbors for " << *f << ": ";
 			for (std::list<int>::iterator it = this->adj[*f].begin(), end = this->adj[*f].end(); it != end; it++) {
 				if (parents[*it] == -1) {
 					parents[*it] = *f;
-					std::cout << *it << " ";
 				}
 			}
-			std::cout << std::endl;
 		}
 
 		cilk_for (std::vector<int>::iterator f = frontier.begin(), end = frontier.end(); f != end; f++) {
+			levels[*f] = level;
 			for (std::list<int>::const_iterator it = this->adj[*f].begin(), end = this->adj[*f].end(); it != end; it++) {
 				if (parents[*it] == *f) {
 					new_frontier->push_back(*it);
@@ -88,6 +88,10 @@ void Graph::PBFS(int s) {
 		frontier.clear();
 		new_frontier.move_out(frontier);
 		level++;
+	}
+
+	for (int i = 0; i < this->V; i++) {
+		std::cout << i << ": " << levels[i] << std::endl;
 	}
 }
 
